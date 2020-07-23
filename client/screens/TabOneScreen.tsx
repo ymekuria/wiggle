59e6a4a147c926a33ac2998c34d9de9b0f17e38d
Yoni <ymekuria@gmail.com>
2020-07-23 13:08:24 -0700

    add sms dependencies

--- client/screens/TabOneScreen.tsx
+++ client/screens/TabOneScreen.tsx
@@ -3,8 +3,9 @@ import { StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { Text, View } from '../components/Themed';
+import _default from '@react-navigation/bottom-tabs/lib/typescript/src/navigators/createBottomTabNavigator';
 
-export default function TabOneScreen() {
+const TabOneScreen: React.FC = () => {
   return (
     <LinearGradient
       colors={['rgba(163,175,243,1)', 'rgba(220,182,232,1)']}
@@ -13,7 +14,7 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
     </LinearGradient>
   );
-}
+};
 
 // background-image: linear-gradient( 89.2deg,  rgba(191,241,236,1) 22.3%, rgba(109,192,236,1) 84.1% );
 
@@ -28,3 +29,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
   }
 });
+
+export default TabOneScreen;
