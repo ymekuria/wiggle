7ff169c9ac7174e544f37d4c96111f70a8e2074d
Yoni <ymekuria@gmail.com>
2020-08-13 15:56:23 -0700

    add getRandmDogPic method to dogPic rest data source

--- src/dataSources/dogPics.ts
+++ src/dataSources/dogPics.ts
@@ -1,5 +1,15 @@
-import { RESTDataSource } from 'apollo-datasource-rest';
+import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 
-class DogAPI extends RESTDataSource {}
+class DogAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://dog.ceo/api/';
+  }
+
+  async getRandomDogPic() {
+    const response = await this.get('/breeds/image/random');
+    return response;
+  }
+}
 
 export default DogAPI;
