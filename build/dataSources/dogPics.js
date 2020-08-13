57d053ffa1b8023776bdb4841e595fda00b1dc1f
Yoni <ymekuria@gmail.com>
2020-08-13 15:46:10 -0700

    add dataSources folder and dog rest api datasource file

---
+++ build/dataSources/dogPics.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_datasource_rest_1 = require("apollo-datasource-rest");
+class DogAPI extends apollo_datasource_rest_1.RESTDataSource {
+}
+exports.default = DogAPI;
---
+++ src/dataSources/dogPics.ts
@@ -0,0 +1,5 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+class DogAPI extends RESTDataSource {}
+
+export default DogAPI;
