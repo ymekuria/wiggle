2e2e391360c172eb5421b54b8f443970ff3539c6
Yoni <ymekuria@gmail.com>
2020-08-24 12:51:13 -0700

    remove build from repo

--- build/services/twilio.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// import { Twilio } from 'twilio';
-// import { twillioAccountSid, twillioAuthToken } from '../config/keys';
-// export const client = new Twilio(twillioAccountSid, twillioAuthToken);
