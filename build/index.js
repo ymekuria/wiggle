ba45630ce282a59b5bf5170380799418174966dc
Yoni <ymekuria@gmail.com>
2020-08-24 12:43:39 -0700

    update  gitignore

--- build/index.js
+++ build/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -33,25 +24,6 @@ app.use(body_parser_1.default.urlencoded({ extended: true }));
 // app.use(authRouter);
 const PORT = process.env.PORT || 3000;
 const prisma = new client_1.PrismaClient();
-const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const newWiggle = yield prisma.wiggle.create({
-        data: {
-            schedule: 'help',
-            user: {
-                connect: { userName: 'test' }
-            },
-            contact: {
-                create: { phoneNumber: '720334444' }
-            }
-        }
-    });
-    console.log('newWiggle', newWiggle);
-});
-main()
-    .catch((e) => console.log(e))
-    .finally(() => __awaiter(void 0, void 0, void 0, function* () {
-    yield prisma.$disconnect();
-}));
 const server = new apollo_server_express_1.ApolloServer({
     typeDefs: schema_1.default,
     resolvers: resolvers_1.default,
