78f639aa24bcfe3afb351424ae81d42db697520f
Yoni <ymekuria@gmail.com>
2020-09-29 16:13:19 -0700

    update TabOneScreen

--- client/screens/TabOneScreen.tsx
+++ client/screens/TabOneScreen.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Platform, Button } from 'react-native';
+import { StyleSheet, Platform, Button, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import * as Facebook from 'expo-facebook';
 
-import {
-  FACEBOOK_APP_ID,
-  AUTH0_CLIENT_ID,
-  AUTHORIZATION_ENDPOINT
-} from '../utils/config';
 import axios from 'axios';
 import * as SMS from 'expo-sms';
 import * as AuthSession from 'expo-auth-session';
+
 import jwtDecode from 'jwt-decode';
 import { Text, View } from '../components/Themed';
 import _default from '@react-navigation/bottom-tabs/lib/typescript/src/navigators/createBottomTabNavigator';
@@ -18,66 +14,12 @@ import useIsSMSAvailable from '../hooks/useIsSMSavailable';
 
 const useProxy = Platform.select({ web: false, default: true });
 const redirectUri = AuthSession.makeRedirectUri({ useProxy });
-console.log(`Redirect URL: ${redirectUri}`); //https://auth.expo.io/@ymekuria/wiggle
+console.log(`Redirect URL: ${redirectUri}`);
+
 const TabOneScreen = () => {
   const isSMSavailable = useIsSMSAvailable();
-  const [name, setName] = useState(null);
 
-  const [request, result, promptAsync] = AuthSession.useAuthRequest(
-    {
-      redirectUri,
-      clientId: AUTH0_CLIENT_ID,
-      // id_token will return a JWT token
-      responseType: 'id_token',
-      // retrieve the user's profile
-      scopes: ['openid', 'profile', 'email'],
-      extraParams: {
-        // ideally, this will be a random value
-        nonce: 'nonce'
-      }
-    },
-    { authorizationEndpoint: AUTHORIZATION_ENDPOINT }
-  );
-
-  // Retrieve the redirect URL, add this to the callback URL list
-  // of your Auth0 application.
-  console.log(`Redirect URL: ${redirectUri}`);
-
-  useEffect(() => {
-    if (result) {
-      if (result.error) {
-        Alert.alert(
-          'Authentication error',
-          result.params.error_description || 'something went wrong'
-        );
-        return;
-      }
-      if (result.type === 'success') {
-        // Retrieve the JWT token and decode it
-        const jwtToken = result.params.id_token;
-        const decoded = jwtDecode(jwtToken);
-
-        console.log({ decoded });
-        const { name } = decoded;
-        setName(name);
-      }
-    }
-  }, [result]);
-
-  const handleClick = async () => {
-    console.log('click');
-    const jwtToken = result?.params?.id_token;
-    const headerConfig = {
-      headers: {
-        Authorization: `Bearer ${jwtToken}`
-      }
-    };
-    let testResponse = await axios.get(
-      'http://localhost:3000/authtest',
-      headerConfig
-    );
-    console.log('Testresponse', testResponse.data);
-  };
+  const handleClick = async () => {};
 
   return (
     <LinearGradient
@@ -88,9 +30,8 @@ const TabOneScreen = () => {
         <Text style={styles.title}>You are logged in, {name}!</Text>
       ) : (
         <Button
-          disabled={!request}
           title="Log in with Auth0"
-          onPress={() => promptAsync({ useProxy })}
+          onPress={() => console.log('click')}
         />
       )}
 
@@ -100,8 +41,6 @@ const TabOneScreen = () => {
     </LinearGradient>
   );
 };
-
-// background-image: linear-gradient( 89.2deg,  rgba(191,241,236,1) 22.3%, rgba(109,192,236,1) 84.1% );
 
 const styles = StyleSheet.create({
   container: {
