e378a1bdbbfb0c9251865d7cc8fe304092545b21
Yoni <ymekuria@gmail.com>
2020-10-08 14:54:24 -0700

    add access token to apollo client request headers

--- client/utils/createApolloClient.ts
+++ client/utils/createApolloClient.ts
@@ -5,14 +5,28 @@ import {
   concat,
   ApolloLink
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { useAuth0 } from '../hooks/useAuth0';
+
+// add dynamic url for testing and production environments
 const httpLink = new HttpLink({ uri: 'http://localhost:8088/graphql' });
 const { accessToken } = useAuth0();
 
-const getApolloClient = (accessToken: string | undefined) => {};
+const getApolloClient = (accessToken: string | undefined) => {
+  const authLink = setContext(async (_, { headers }) => {
+    if (!accessToken) {
+      return { headers };
+    }
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${accessToken}`
+      }
+    };
+  });
 
-// add dynamic url for testing and production environments
-export const apolloClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: concat(checkAuth, httpLink)
-});
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: concat(authLink, httpLink)
+  });
+};
