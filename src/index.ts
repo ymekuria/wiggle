cdcd582dcf274c78a13ba6caba2829d98da4cce5
Yoni <ymekuria@gmail.com>
2020-11-10 15:13:25 -0800

    create seperate validateUser middleware file

--- src/index.ts
+++ src/index.ts
@@ -33,39 +33,7 @@ app.use(cors());
 // app.use(authRouter);
 app.use(checkJwt);
 
-app.use(async (req, res, next) => {
-  console.log('testing middleware req.use', req.user);
-  if (req.user?.sub) {
-    console.log('JWT token valid and is req.user');
-    const dbUser = await prisma.user.findOne({ where: { id: req.user.sub } });
-
-    if (!dbUser) {
-      console.log('user ISNT in the db. Adding user to DB');
-      const newUser = await prisma.user
-        .create({
-          data: {
-            id: req.user.sub
-          }
-        })
-        .then((newUser) => {
-          console.log('new user created', newUser);
-          next();
-        })
-        .catch((err) => {
-          console.log(err);
-          next();
-        });
-
-      // await next();
-    } else {
-      console.log('user IS IN DB. DbUser =', dbUser);
-      next();
-    }
-  } else {
-    console.log('no token sent. req.user', req.user);
-    next();
-  }
-});
+app.use(validateUser);
 
 // app.use((err, req, res, next) => {
 //   console.log(err);
