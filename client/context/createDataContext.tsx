0af78c8d10cbfe8ceb96b70f1f2ad081c81ec826
Yoni <ymekuria@gmail.com>
2020-12-29 17:17:50 -0800

    create generic helper function to create a react context

--- client/context/createDataContext.tsx
+++ client/context/createDataContext.tsx
@@ -1,1 +1,15 @@
+import React, { useReducer, createContext } from 'react';
 
+export default (reducer, actions, initialState) => {
+  const DataContext = createContext();
+
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+      <DataContext.Provider value={{ state }}>{children}</DataContext.Provider>
+    );
+  };
+
+  return { DataContext, Provider };
+};
